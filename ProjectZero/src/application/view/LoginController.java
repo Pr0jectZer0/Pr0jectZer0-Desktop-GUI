9d91451d65966c45834b5638c4f91ab1e2b19845
Marvin Kramer <marvinkramer95@gmail.com>
2017-12-01 00:07:31 +0100

    Registrierung und Anmeldung - Funktionen hinzugefügt

--- ProjectZero/src/application/view/LoginController.java
+++ ProjectZero/src/application/view/LoginController.java
@@ -3,7 +3,12 @@ package application.view;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
 import application.Main;
+import application.model.HttpPostRequest;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,6 +17,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.effect.SepiaTone;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
@@ -27,6 +33,10 @@ public class LoginController {
 	private Label minimizeL;
 	@FXML
 	private Label closeL;
+	@FXML
+	private JFXTextField tFUsernameEmail;
+	@FXML
+	private JFXPasswordField tFpw;
 	private Main main;
 	private Stage mainStage;
 
@@ -41,19 +51,77 @@ public class LoginController {
 	@FXML
 	private void signIn() {
 		try {
-			mainStage = new Stage();
+			String[][] parameter = {{ "email", tFUsernameEmail.getText() }, {"password", tFpw.getText()}};
+			String response = HttpPostRequest.send("user/login", parameter);
+			if (response.contains("token")) {
+				try {
+					mainStage = new Stage();
+					FXMLLoader loader = new FXMLLoader();
+					loader.setLocation(Main.class.getResource("view/MainLayout.fxml"));
+					AnchorPane mainAnchor = (AnchorPane) loader.load();
+					Scene scene = new Scene(mainAnchor);
+					mainStage.setScene(scene);
+					mainStage.show();
+					main.getLoginStage().close();
+				} catch (Exception e) {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Fehler!");
+					alert.setHeaderText("Es gab ein Fehler bei der Anmeldung!");
+					alert.setContentText("Bitte wenden Sie sich an den Support.");
+					alert.initOwner(main.getLoginStage());
+
+					StringWriter sw = new StringWriter();
+					PrintWriter pw = new PrintWriter(sw);
+					e.printStackTrace(pw);
+					String exceptionText = sw.toString();
+
+					Label label = new Label("The exception stacktrace was:");
+
+					TextArea textArea = new TextArea(exceptionText);
+					textArea.setEditable(false);
+
+					textArea.setMaxWidth(Double.MAX_VALUE);
+					textArea.setMaxHeight(Double.MAX_VALUE);
+					GridPane.setVgrow(textArea, Priority.ALWAYS);
+					GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+					GridPane expContent = new GridPane();
+					expContent.setMaxWidth(Double.MAX_VALUE);
+					expContent.add(label, 0, 0);
+					expContent.add(textArea, 0, 1);
+					alert.getDialogPane().setExpandableContent(expContent);
+					alert.showAndWait();
+				}
+			} else {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Fehler!");
+				alert.setHeaderText("Es gab ein Fehler bei der Anmeldung!");
+				alert.setContentText("Falsche Login-Daten.");
+				alert.initOwner(main.getLoginStage());
+				alert.showAndWait();
+			}
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+	}
+	
+	@FXML
+	private void register() {
+		try {
+			Stage registerStage = new Stage();
 			FXMLLoader loader = new FXMLLoader();
-			mainStage.initStyle(StageStyle.TRANSPARENT);
-			loader.setLocation(Main.class.getResource("view/MainLayout.fxml"));
-			AnchorPane mainAnchor = (AnchorPane) loader.load();
-			Scene scene = new Scene(mainAnchor);
-			mainStage.setScene(scene);
-			mainStage.show();
-			main.getLoginStage().close();
+			loader.setLocation(Main.class.getResource("view/Registrierung.fxml"));
+			AnchorPane registerAnchor = (AnchorPane) loader.load();
+			Scene scene = new Scene(registerAnchor);
+			registerStage.initOwner(main.getLoginStage());
+			Image image = new Image("application/data/images/logo.png");
+			registerStage.getIcons().add(image);
+			registerStage.setScene(scene);
+			registerStage.show();
 		} catch (Exception e) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Fehler!");
-			alert.setHeaderText("Es gab ein Fehler bei der Anmeldung!");
+			alert.setHeaderText("Es gab ein Fehler beim Öffnen des Registrier-Fensters!");
 			alert.setContentText("Bitte wenden Sie sich an den Support.");
 			alert.initOwner(main.getLoginStage());
 
---
+++ ProjectZero/src/application/view/RegistrierungController.java
@@ -0,0 +1,73 @@
+package application.view;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import application.model.HttpPostRequest;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+public class RegistrierungController {
+	@FXML
+	private JFXTextField tFUsername;
+	@FXML
+	private JFXTextField tFEmail;
+	@FXML
+	private JFXPasswordField tFPw;
+	@FXML
+	private JFXPasswordField tFPw2;
+	@FXML
+	private JFXButton bRegister;
+	
+	@FXML
+	private void register() {
+		String[][] parameter = {{ "email", tFEmail.getText() }, {"name", tFUsername.getText()}, {"password", tFPw.getText()}};
+		try{
+			HttpPostRequest.send("user/", parameter);
+		}
+		catch(Exception e)
+		{
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Fehler!");
+			alert.setHeaderText("Es gab ein Fehler beim Registrieren!");
+			alert.setContentText("Bitte wenden Sie sich an den Support.");
+
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			String exceptionText = sw.toString();
+
+			Label label = new Label("The exception stacktrace was:");
+
+			TextArea textArea = new TextArea(exceptionText);
+			textArea.setEditable(false);
+
+			textArea.setMaxWidth(Double.MAX_VALUE);
+			textArea.setMaxHeight(Double.MAX_VALUE);
+			GridPane.setVgrow(textArea, Priority.ALWAYS);
+			GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+			GridPane expContent = new GridPane();
+			expContent.setMaxWidth(Double.MAX_VALUE);
+			expContent.add(label, 0, 0);
+			expContent.add(textArea, 0, 1);
+			alert.getDialogPane().setExpandableContent(expContent);
+			alert.showAndWait();
+		}
+	}
+}
