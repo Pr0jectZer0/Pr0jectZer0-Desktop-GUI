4f1e7e20fe83707528489903a19a2cd6d29f3910
Marvin Kramer <marvinkramer95@gmail.com>
2017-12-04 23:26:34 +0100

    Freundesliste-Slider erstellt

--- ProjectZero/src/application/view/MainLayoutController.java
+++ ProjectZero/src/application/view/MainLayoutController.java
@@ -1,10 +1,57 @@
 package application.view;
 
+import java.beans.EventHandler;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
 /**
  * Main-Controller-Klasse
  * 
  * @author Dorsch, Paul, Deutsch, Penner, Kramer
  */
 public class MainLayoutController {
+	@FXML
+	private JFXDrawer drawer;
+	@FXML
+	private JFXHamburger hamburger;
+	
+	@FXML
+	private void initialize() {
+		initFreundesliste();
+	}
 
+	private void initFreundesliste() {
+		try {
+		ScrollPane freundesliste = FXMLLoader.load(getClass().getResource("Freundesliste.fxml"));
+		drawer.setSidePane(freundesliste);
+		} catch (Exception e) {
+			
+		}
+		HamburgerBasicCloseTransition transition = new HamburgerBasicCloseTransition(hamburger);
+		transition.setRate(-1);
+		hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+			transition.setRate(transition.getRate() * -1);
+			transition.play();
+			if (drawer.isShown()) {
+				drawer.close();
+			} else
+				drawer.open();
+		});
+	}
+	
+	@FXML
+	private void test() {
+		System.out.println("test");
+	}
 }
