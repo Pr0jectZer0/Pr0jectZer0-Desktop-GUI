7ed0aa69de563e30ec62c4584fdbe6196c2afbb7
Luisnator <LuisDeutsch98@gmail.com>
2017-12-14 17:04:49 +0100

    fix kram

--- ProjectZero/src/application/view/PopupController.java
+++ ProjectZero/src/application/view/PopupController.java
@@ -14,55 +14,45 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class PopupController
-{
+public class PopupController {
 	@FXML
 	private AnchorPane panemain;
 	@FXML
 	private JFXButton btnchat;
 	private Stage popupstage;
-	
+
 	@FXML
-	private void initialize()
-	{
+	private void initialize() {
 		this.popupstage = FreundeslisteController.getPopupstage();
-		panemain.setOnMouseExited(new EventHandler<MouseEvent>()
-		{
+		panemain.setOnMouseExited(new EventHandler<MouseEvent>() {
 
 			@Override
-			public void handle(MouseEvent event)
-			{
+			public void handle(MouseEvent event) {
 				popupstage.hide();
 			}
 		});
 		this.btnchat.setOnMouseClicked(new EventHandler<MouseEvent>() {
-
 			@Override
 			public void handle(MouseEvent event) {
-				if(event.getEventType() == MouseEvent.MOUSE_CLICKED)
-				{
-					try{
+				if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+					try {
 						Stage chatstage = new Stage();
 						FXMLLoader loader = new FXMLLoader();
 						loader.setLocation(getClass().getResource("Chat.fxml"));
 						chatstage.setTitle("Platzhalter");
 						Image image = new Image("application/data/images/logo.png");
 						chatstage.getIcons().add(image);
-						//chatstage.initStyle(StageStyle.UNDECORATED);
+						// chatstage.initStyle(StageStyle.UNDECORATED);
 						AnchorPane chatAnchor = (AnchorPane) loader.load();
 						Scene scene = new Scene(chatAnchor);
 						chatstage.setScene(scene);
-						}
-						catch(Exception e)
-						{
-							e.printStackTrace();
-						}
+						chatstage.show();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
-				
 			}
 		});
 	}
-	
-	
-	
+
 }
