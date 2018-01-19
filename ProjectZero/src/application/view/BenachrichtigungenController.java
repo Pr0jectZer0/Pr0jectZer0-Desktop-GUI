b8c4fbd2c6d336e3feb17da395d9e39d3a21ec31
Marvin Kramer <marvinkramer95@gmail.com>
2018-01-19 07:40:50 +0100

    API für Benachrichtigungen erstellt

--- ProjectZero/src/application/view/BenachrichtigungenController.java
+++ ProjectZero/src/application/view/BenachrichtigungenController.java
@@ -1,9 +1,12 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class BenachrichtigungenController {
-
+	@FXML
+	private VBox benachrichtigungenBox;
+	
 	@FXML
 	private void initialize() {
 		
