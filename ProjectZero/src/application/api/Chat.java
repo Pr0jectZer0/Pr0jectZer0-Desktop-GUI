d11c4aca17d73bf2fef1e7ac36bdb0f5a7d57504
Steffen Dorsch <s@dorsch.it>
2018-01-20 15:13:45 +0100

    Message Model hinzugefügt

--- ProjectZero/src/application/api/Chat.java
+++ ProjectZero/src/application/api/Chat.java
@@ -1,18 +1,61 @@
 package application.api;
 
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import application.model.HttpWebRequest;
+
 public class Chat {
 	
 	private Chat() {}
 	
-	public static String[][] getMessages(String user_id, String token)
+	public static int getChatroomID(int userID) {
+		try {
+			return new JSONObject(HttpWebRequest.sendGetRequest("chatroom/" + userID + "?token=" + User.getLoginToken())).getInt("chatroom");
+		} catch (JSONException | IOException e) {
+			return -1;
+		}
+	}
+	public static String[][] getMessages(String chatID)
 	{
-		//TODO: fully implement
-		return new String[][] { { "pr0jectzer0.ml/api/chatroom/" + user_id + "?token=" + User.getLoginToken() }, {""} };
+			String name = "";
+			String mess = "";
+			//System.out.println(HttpWebRequest
+			//		.sendGetRequest("chatroom/" + chatID + "/messages" + "?token=" + User.getLoginToken()));
+			// message = (JsonObject) new JsonParser().parse(HttpWebRequest
+			//		.sendGetRequest("chatroom/" + chatID + "/messages" + "?token=" + User.getLoginToken()));
+			//JsonArray messages = (JsonArray) message.get("message");
+			//Iterator<JsonElement> iterator = messages.iterator();
+			//while (iterator.hasNext())
+			//{
+			//	JsonObject messobj = (JsonObject) iterator.next();
+			//	JsonObject userobj = (JsonObject) messobj.get("user");
+			//	name = userobj.get("name").toString();
+			//	mess += name.replace("\"", "") + ": " + messobj.get("message").toString().replace("\"", "") + "\n";
+
+			//}
+			//return mess;
+		return new String[][] { { "pr0jectzer0.ml/api/chatroom/" + chatID + "?token=" + User.getLoginToken() } };
 	}
 	
-	public static boolean sendMessage(String message, String token)
+	public static boolean sendMessage(int chatID, String message)
 	{
-		//TODO: fully implement
-		return message.contains("");
+		if (message == null || message.isEmpty())
+			return false;
+		String [][] parameter = { { "message", message } };
+		try {
+			String response = HttpWebRequest.sendPostRequest("chatroom/" + chatID + "/messages?token=" + User.getLoginToken(), parameter);
+			return response.contains("Nachricht wurde gesendet.");
+		} catch (IOException e) {
+			return false;
+		}
 	}
 }
--- ProjectZero/test/application/api/ChatTest.java
+++ ProjectZero/test/application/api/ChatTest.java
@@ -1,5 +1,53 @@
 package application.api;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ChatTest {
 
+	private static int friendID = 14,
+			chatID = 33;
+	@Before
+	public void setUp() {
+		User.login("peter@peter.com", "Peter123");
+	}
+	
+	@Test
+	public void getChatIDValid() {
+		assertEquals(Chat.getChatroomID(friendID), chatID);
+	}
+	
+	@Test
+	public void getChatIDInvalid() {
+		assertEquals(Chat.getChatroomID(-1), -1);
+	}
+	
+	@Test
+	public void getChatIDNotExisting() {
+		assertEquals(Chat.getChatroomID(131231237), -1);
+	}
+	
+	@Test
+	public void sendMessageValid() {
+		assertTrue(Chat.sendMessage(chatID, "JUNIT_TEST"));
+	}
+	
+	@Test
+	public void sendMessageNull() {
+		assertFalse(Chat.sendMessage(chatID, null));
+	}
+	
+	@Test
+	public void sendMessageEmpty() {
+		assertFalse(Chat.sendMessage(chatID, ""));
+	}
+	
+	@Test
+	public void getMessages() {
+		
+	}
 }
