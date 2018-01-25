668699a827d61616ff538ff42fe0cc58303595ba
Luisnator <LuisDeutsch98@gmail.com>
2018-01-25 12:29:57 +0100

    BenachrichtigungsController-Fix2

--- ProjectZero/src/application/view/BenachrichtigungenController.java
+++ ProjectZero/src/application/view/BenachrichtigungenController.java
@@ -21,44 +21,47 @@ public class BenachrichtigungenController {
 
 	@FXML
 	private void initialize() {
-		initBenachrichtigungen();
+	initBenachrichtigungen();
 	}
 
 	private void initBenachrichtigungen() {
 		try {
 			ObservableList<application.model.Benachrichtigungen> benachrichtigungen = Benachrichtigungen.getBenachrichtigungen();
-			
-			for (int i = 0; i < benachrichtigungen.size(); i++) {
+			if(benachrichtigungen != null)
+			{
+				for (int i = 0; i < benachrichtigungen.size(); i++) {
 
-				Label label = new Label();
-				label.setText("Freundschaftsanfrage von " + benachrichtigungen.get(i).getUserName() + "(Id: "
-						+ benachrichtigungen.get(i).getUserId() + "):");
+					Label label = new Label();
+					label.setText("Freundschaftsanfrage von " + benachrichtigungen.get(i).getUserName() + "(Id: "
+							+ benachrichtigungen.get(i).getUserId() + "):");
 
-				JFXButton accept = new JFXButton("Akzeptieren");
-				JFXButton decline = new JFXButton("Ablehnen");
-				accept.getStyleClass().add("login-button");
-				accept.setStyle(accept.getStyle() + "-fx-text-fill: #B2B2B2;");
-				decline.getStyleClass().add("login-button");
-				decline.setStyle(decline.getStyle() + "-fx-text-fill: #B2B2B2;");
+					JFXButton accept = new JFXButton("Akzeptieren");
+					JFXButton decline = new JFXButton("Ablehnen");
+					accept.getStyleClass().add("login-button");
+					accept.setStyle(accept.getStyle() + "-fx-text-fill: #B2B2B2;");
+					decline.getStyleClass().add("login-button");
+					decline.setStyle(decline.getStyle() + "-fx-text-fill: #B2B2B2;");
 
-				DropShadow shadow = new DropShadow();
-				shadow.setBlurType(BlurType.TWO_PASS_BOX);
-				shadow.setColor(Color.RED);
-				
-				HBox hBox = new HBox();
-				hBox.setEffect(shadow);
-				hBox.setSpacing(20);
-				hBox.getChildren().addAll(label, accept, decline);
-				
-				VBox vBox = new VBox();
-				vBox.setSpacing(20);
-				vBox.setStyle(vBox.getStyle() + "-fx-background-color: derive(#2A2E37, 20.0%);");
-				vBox.getStylesheets().add("application/data/css/ProjectZero_Theme.css");
-				vBox.setPadding(new Insets(10, 0, 10, 10));
-				vBox.getChildren().addAll(hBox);
-								
-				benachrichtigungenBox.getChildren().add(vBox);
+					DropShadow shadow = new DropShadow();
+					shadow.setBlurType(BlurType.TWO_PASS_BOX);
+					shadow.setColor(Color.RED);
+					
+					HBox hBox = new HBox();
+					hBox.setEffect(shadow);
+					hBox.setSpacing(20);
+					hBox.getChildren().addAll(label, accept, decline);
+					
+					VBox vBox = new VBox();
+					vBox.setSpacing(20);
+					vBox.setStyle(vBox.getStyle() + "-fx-background-color: derive(#2A2E37, 20.0%);");
+					vBox.getStylesheets().add("application/data/css/ProjectZero_Theme.css");
+					vBox.setPadding(new Insets(10, 0, 10, 10));
+					vBox.getChildren().addAll(hBox);
+									
+					benachrichtigungenBox.getChildren().add(vBox);
+				}
 			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
