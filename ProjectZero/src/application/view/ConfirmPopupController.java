b4e91d0495332ec25499a49ca232b89cfcdd062a
Phillip51 <phillip.penner@fh-bielefeld.de>
2018-01-25 19:38:09 +0100

    ConfirmPopup hinzugefügt und umgesetzt, NotizController erweitert

---
+++ ProjectZero/src/application/view/ConfirmPopupController.java
@@ -0,0 +1,35 @@
+package application.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class ConfirmPopupController {
+
+	@FXML
+	Label lblprim;
+	@FXML 
+	Label lblsec;
+	
+	private Stage stage;
+	
+	@FXML
+	private void initialize() {
+		this.stage = NotizController.getNotizController().getConfStage();
+		this.stage.setResizable(false);
+		this.stage.setTitle("Speichern");
+		this.stage.show();
+	}
+	
+	@FXML
+	private void jaButtonAction() {
+		NotizController.getNotizController().saveExtern();
+		this.stage.close();
+	}
+	
+	@FXML
+	private void neinButtonAction() {
+		NotizController.getNotizController().dontSave();
+		this.stage.close();
+	}
+}
--- ProjectZero/src/application/view/NotizController.java
+++ ProjectZero/src/application/view/NotizController.java
@@ -7,69 +7,101 @@ import org.json.JSONException;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 
+import application.Main;
 import application.api.Notes;
 import application.api.User;
 import application.model.ErrorWindow;
 import application.model.Note;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class NotizController {
 	@FXML
-	JFXTextArea selectedNoteText;
+	private JFXTextArea selectedNoteText;
 	@FXML
-	JFXTextArea selectedNoteTitle;
+	private JFXTextArea selectedNoteTitle;
 	
 	@FXML
-	TableColumn<Note, String> noteNameCol;
+	private TableColumn<Note, String> noteNameCol;
 	@FXML
-	TableColumn<Note, Integer> noteIdCol;
+	private TableColumn<Note, Integer> noteIdCol;
 	
 	@FXML
-	TableView<Note> noteList;
+	private TableView<Note> noteList;
 	@FXML
-	TableView<User> memberList;
+	private TableView<User> memberList;
 	
 	@FXML
-	TableColumn<User, String> memberNameCol;
+	private TableColumn<User, String> memberNameCol;
 	@FXML
-	TableColumn<User, Integer> memberIdCol;
+	private TableColumn<User, Integer> memberIdCol;
 	
 	@FXML
-	JFXButton btnSelectNote;
+	private JFXButton btnSelectNote;
 	@FXML
-	JFXButton btnNewNote;
+	private JFXButton btnNewNote;
 	@FXML
-	JFXButton btnSaveNote;
+	private JFXButton btnSaveNote;
+	
+	@FXML
+	private Stage confStage;
+	
+	private AnchorPane popAnchor;
 	
 	
 	//-3 = Notiz offen und gespeichert, -2 = noch keine Notiz geï¿½ffnet, -1 = neue Notiz offen NICHT gespeichert, rest = ID der Notiz (NICHT gespeichert)
-	int currentNoteId = -2;
-	String currentNoteTitle;
-	String currentNoteText;
-	Note currentNote;
+	private int currentNoteId = -2;
+	private String currentNoteTitle;
+	private String currentNoteText;
+	private Note currentNote;
+	
+	private static NotizController notizcontroller;
 	
 	/**
 	 * Initialisierungen
 	 */
 	@FXML
 	private void initialize() {
+		notizcontroller = this;
 		initNoteList();
 	}
 	
 	private void initNoteList() {
 		try {
 			noteList.setItems(Notes.getNotesFromUser());
+			newNoteAction();
 		} catch (Exception e) {
 			ErrorWindow.newErrorWindow("Es gab ein Fehler beim Hinzufï¿½gen aller Notizen!", (Stage) noteList.getScene().getWindow(), e);
 		}
 		noteIdCol.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getID()).asObject());
 		noteNameCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+		
+		noteList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if (event.getEventType() == MouseEvent.MOUSE_CLICKED && event.getButton() == MouseButton.PRIMARY) {
+					try {
+						selectNoteAction();
+					} catch (Exception e) {
+						ErrorWindow.newErrorWindow("Es gab ein Fehler beim öffnen der Notiz!", (Stage) noteList.getScene().getWindow(), e);
+					}
+
+				}
+			}
+		});
 	}
 	
 	@FXML
@@ -104,9 +136,10 @@ public class NotizController {
 			if(!(selectedNoteTitle.getText().equals(currentNoteTitle)) || !(selectedNoteText.getText().equals(currentNoteText))) {
 				try {
 					Note tempNote = Notes.changeNote(selectedNoteTitle.getText(), selectedNoteText.getText(), currentNoteId);
-					noteList.getItems().add(tempNote);
-					currentNote.setTitle(currentNoteTitle = tempNote.getTitle());
-					currentNote.setText(currentNoteText = tempNote.getText());
+					noteList.getItems().set(noteList.getItems().indexOf(currentNote), tempNote);
+					/*currentNote.setTitle(currentNoteTitle = tempNote.getTitle());
+					currentNote.setText(currentNoteText = tempNote.getText());*/
+					currentNote = tempNote;
 					currentNoteId = -3;
 					currentNoteTitle = tempNote.getTitle();
 					currentNoteText = tempNote.getText();
@@ -129,7 +162,52 @@ public class NotizController {
 			currentNoteTitle = tempNote.getTitle();
 			currentNoteText = tempNote.getText();
 		}else {
-			
+			loadConfirmationPopup();
 		}
+	}
+	@FXML
+	private void deleteNoteAction() {
+		if(currentNoteId == -1) {
+			selectedNoteTitle.setText("Unbenannte Notiz");
+			selectedNoteText.setText("");
+			currentNoteTitle = "Unbenannte Notiz";
+			currentNoteText = "";
+			currentNoteId = -1;
+		}else if(currentNoteId == -3 || currentNoteId > -1) {
+			Notes.deleteNote(currentNote.getID());
+			selectedNoteTitle.setText("Unbenannte Notiz");
+			selectedNoteText.setText("");
+			currentNoteTitle = "Unbenannte Notiz";
+			currentNoteText = "";
+			currentNoteId = -1;
+		}
+	}
+	private void loadConfirmationPopup() {
+		try {
+			confStage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("ConfirmPopup.fxml"));
+			confStage.initStyle(StageStyle.UNDECORATED);
+			popAnchor = (AnchorPane) loader.load();
+			Scene scene = new Scene(popAnchor);
+			confStage.setScene(scene);
+			confStage.setY(selectedNoteTitle.getLayoutY()+300);
+			confStage.setX(selectedNoteTitle.getLayoutX()+700);
+		} catch (Exception e) {
+			ErrorWindow.newErrorWindow("Es gab ein Fehler beim öffnen des Popup-Fensters!", (Stage) noteList.getScene().getWindow(), e);
+		}
+	}
+	public Stage getConfStage() {
+		return this.confStage;
+	}
+	public static NotizController getNotizController() {
+		return notizcontroller;
+	}
+	public void saveExtern() {
+		saveNoteAction();
+	}
+	public void dontSave() {
+		selectedNoteTitle.setText(currentNoteTitle);
+		selectedNoteText.setText(currentNoteText);
 	}
 }
