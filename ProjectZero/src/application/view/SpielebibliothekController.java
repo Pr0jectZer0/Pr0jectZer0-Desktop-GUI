b26084dbeddb2c1ae4f8b6729da8a40364590b56
Steffen Dorsch <s@dorsch.it>
2018-01-25 19:58:31 +0100

    Spielebibliothek überarbeitet

--- ProjectZero/src/application/view/SpielebibliothekController.java
+++ ProjectZero/src/application/view/SpielebibliothekController.java
@@ -2,28 +2,24 @@ package application.view;
 
 import java.io.IOException;
 
-import org.json.JSONObject;
+import org.json.JSONException;
 
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
 
+import application.api.UserGames;
 import application.model.Game;
-import application.model.HttpWebRequest;
 import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class SpielebibliothekController {
 	@FXML
-	private JFXListView<Object> gamelist;
+	private JFXListView<Game> gamelist;
 	@FXML
 	private Label selectedGameName;
 	@FXML
 	private JFXTextArea selectedGameDescription;
-	private static ObservableList<Game> games = FXCollections.observableArrayList();
-	private static ObservableList<Integer> subgames = FXCollections.observableArrayList();
 
 	@FXML
 	private void initialize() {
@@ -32,39 +28,18 @@ public class SpielebibliothekController {
 	}
 
 	private void initGames() {
-//		try {
-//			JSONObject response = new JSONObject(
-//					HttpWebRequest.sendGetRequest("user/game/list?token=" + LoginController.getToken()));
-//
-//			int anzgames = response.getJSONArray("games").length();
-//
-//			for (int i = 0; i < anzgames; i++) {
-//				games.add(new Game(response.getJSONArray("games").getJSONObject(i).getString("name"),
-//						response.getJSONArray("games").getJSONObject(i).getString("beschreibung"),
-//						response.getJSONArray("games").getJSONObject(i).getInt("id")));
-//				subgames.add(response.getJSONArray("games").getJSONObject(i).getInt("id"));
-//				gamelist.getItems().add(response.getJSONArray("games").getJSONObject(i).getString("name"));
-//			}
-//		} catch (IOException e) {
-//			// TODO
-//		}
+		try {
+			gamelist.setItems(UserGames.getGames());
+		} catch (IOException | JSONException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void initSelectedGame() {
-		gamelist.getSelectionModel().selectedItemProperty().addListener((ChangeListener<Object>) (observable, oldValue, newValue) -> {
-			System.out.println("test");
-			int selectedGame = gamelist.getSelectionModel().getSelectedIndex();
-			int selectedGameId = subgames.get(selectedGame);
-			System.out.println("test4");
-			for (int i = 0; i<games.size(); i++) {
-				System.out.println("test5");
-				if (selectedGameId == games.get(i).getId()) {
-					System.out.println("test6");
-					selectedGameName.setText(games.get(i).getName());
-					selectedGameDescription.setText(games.get(i).getDescrption());
-				}
-			}
-			
+		gamelist.getSelectionModel().selectedItemProperty().addListener((ChangeListener<Game>) (observable, oldValue, newValue) -> {
+			Game selectedGame = gamelist.getSelectionModel().getSelectedItem();
+			selectedGameDescription.setText(selectedGame.getDescrption());
+			selectedGameName.setText(selectedGame.getName());	
 		});
 	}
 
@@ -81,22 +56,16 @@ public class SpielebibliothekController {
 
 	@FXML
 	private void deleteGame() {
-//		try {
-//			if (!gamelist.getSelectionModel().isEmpty()) {
-//				int selectedGame = gamelist.getSelectionModel().getSelectedIndex();
-//				gamelist.getItems().remove(selectedGame);
-//				subgames.remove(selectedGame);
-//				int selectedGameId = games.get(selectedGame).getId();
-//				games.remove(selectedGame);
-//				gamelist.refresh();
-//				System.out.println(selectedGameId);
-//				String response = HttpWebRequest.sendDeleteRequest(
-//						"user/game/remove/" + selectedGameId + "?token=" + LoginController.getToken());
-//				System.out.println(response);
-//			}
-//		} catch (IOException e) {
-//			// TODO
-//		}
+		try {
+			if (!gamelist.getSelectionModel().isEmpty()) {
+				int selectedGame = gamelist.getSelectionModel().getSelectedIndex();
+				UserGames.delete(gamelist.getItems().get(selectedGame).getId());
+				gamelist.getItems().remove(selectedGame);
+				gamelist.refresh();
+			}
+		} catch (IOException e) {
+			System.out.println("Backend Route not working(Server send error 400 - Game not in list)!");
+			//e.printStackTrace();
+		}
 	}
-
 }
