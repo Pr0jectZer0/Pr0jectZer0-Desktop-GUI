1f4e13957b05eadef8ba9dcf68937cf45ed04eac
Steffen Dorsch <s@dorsch.it>
2018-01-25 18:46:38 +0100

    BenachrichtigungsController crash bei keinen Benachrichtigungen fixed

--- ProjectZero/src/application/view/BenachrichtigungenController.java
+++ ProjectZero/src/application/view/BenachrichtigungenController.java
@@ -2,6 +2,8 @@ package application.view;
 
 import java.io.IOException;
 
+import org.json.JSONException;
+
 import com.jfoenix.controls.JFXButton;
 
 import application.api.Benachrichtigungen;
@@ -27,40 +29,39 @@ public class BenachrichtigungenController {
 	private void initBenachrichtigungen() {
 		try {
 			ObservableList<application.model.Benachrichtigungen> benachrichtigungen = Benachrichtigungen.getBenachrichtigungen();
-			
-			for (int i = 0; i < benachrichtigungen.size(); i++) {
-
-				Label label = new Label();
-				label.setText("Freundschaftsanfrage von " + benachrichtigungen.get(i).getUserName() + "(Id: "
-						+ benachrichtigungen.get(i).getUserId() + "):");
-
-				JFXButton accept = new JFXButton("Akzeptieren");
-				JFXButton decline = new JFXButton("Ablehnen");
-				accept.getStyleClass().add("login-button");
-				accept.setStyle(accept.getStyle() + "-fx-text-fill: #B2B2B2;");
-				decline.getStyleClass().add("login-button");
-				decline.setStyle(decline.getStyle() + "-fx-text-fill: #B2B2B2;");
-
-				DropShadow shadow = new DropShadow();
-				shadow.setBlurType(BlurType.TWO_PASS_BOX);
-				shadow.setColor(Color.RED);
-				
-				HBox hBox = new HBox();
-				hBox.setEffect(shadow);
-				hBox.setSpacing(20);
-				hBox.getChildren().addAll(label, accept, decline);
-				
-				VBox vBox = new VBox();
-				vBox.setSpacing(20);
-				vBox.setStyle(vBox.getStyle() + "-fx-background-color: derive(#2A2E37, 20.0%);");
-				vBox.getStylesheets().add("application/data/css/ProjectZero_Theme.css");
-				vBox.setPadding(new Insets(10, 0, 10, 10));
-				vBox.getChildren().addAll(hBox);
-								
-				benachrichtigungenBox.getChildren().add(vBox);
+			if (benachrichtigungen != null) {
+				for (int i = 0; i < benachrichtigungen.size(); i++) {
+					Label label = new Label();
+					label.setText("Freundschaftsanfrage von " + benachrichtigungen.get(i).getUserName() + "(Id: "
+							+ benachrichtigungen.get(i).getUserId() + "):");
+	
+					JFXButton accept = new JFXButton("Akzeptieren");
+					JFXButton decline = new JFXButton("Ablehnen");
+					accept.getStyleClass().add("login-button");
+					accept.setStyle(accept.getStyle() + "-fx-text-fill: #B2B2B2;");
+					decline.getStyleClass().add("login-button");
+					decline.setStyle(decline.getStyle() + "-fx-text-fill: #B2B2B2;");
+	
+					DropShadow shadow = new DropShadow();
+					shadow.setBlurType(BlurType.TWO_PASS_BOX);
+					shadow.setColor(Color.RED);
+					
+					HBox hBox = new HBox();
+					hBox.setEffect(shadow);
+					hBox.setSpacing(20);
+					hBox.getChildren().addAll(label, accept, decline);
+					
+					VBox vBox = new VBox();
+					vBox.setSpacing(20);
+					vBox.setStyle(vBox.getStyle() + "-fx-background-color: derive(#2A2E37, 20.0%);");
+					vBox.getStylesheets().add("application/data/css/ProjectZero_Theme.css");
+					vBox.setPadding(new Insets(10, 0, 10, 10));
+					vBox.getChildren().addAll(hBox);
+									
+					benachrichtigungenBox.getChildren().add(vBox);
+				}
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (IOException | JSONException e) {
 			e.printStackTrace();
 		}
 
