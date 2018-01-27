174fbc032c4af96b0f09683cc4d5ca575cb747e1
Luis D <luis.deutsch@fh-bielefeld.de>
2018-01-27 16:33:33 +0100

    Formatierung2

--- ProjectZero/src/application/api/Friends.java
+++ ProjectZero/src/application/api/Friends.java
@@ -11,81 +11,106 @@ import application.model.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class Friends {
+public class Friends
+{
 
 	private static ObservableList<User> friends = null;
 	private static ObservableList<application.model.FriendRequest> friendRequests = null;
-	
-	private Friends() {}
 
-	public static FriendAddState add(int id) {
-		try {
+	private Friends()
+	{
+	}
+
+	public static FriendAddState add(int id)
+	{
+		try
+		{
 			String[][] parameter = { { "id", Integer.toString(id) } };
 			HttpWebRequest.sendPostRequest("friend/add?token=" + application.api.User.getLoginToken(), parameter);
 			return FriendAddState.Success;
 		}
-		catch (Exception e) {
-			if (e.getMessage().contains("Server returned HTTP response code: 400")) {
+		catch (Exception e)
+		{
+			if (e.getMessage().contains("Server returned HTTP response code: 400"))
+			{
 				return FriendAddState.AlreadyFriends;
 			}
 			e.printStackTrace();
 			return FriendAddState.ServerError;
 		}
 	}
-	
-	public static boolean delete(int id) {
-		try {
-			HttpWebRequest.sendDeleteRequest("friend/remove/" + Integer.toString(id) + "?token=" + application.api.User.getLoginToken());
+
+	public static boolean delete(int id)
+	{
+		try
+		{
+			HttpWebRequest.sendDeleteRequest(
+					"friend/remove/" + Integer.toString(id) + "?token=" + application.api.User.getLoginToken());
 			return true;
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			return false;
 		}
 	}
-	
-	public enum FriendAddState {
-		Success,
-		AlreadyFriends,
-		ServerError,
+
+	public enum FriendAddState
+	{
+		Success, AlreadyFriends, ServerError,
 	}
-	
-	public static ObservableList<User> getFriends() throws JSONException, IOException {
-		if (friends == null) {
-			JSONObject response = new JSONObject(HttpWebRequest.sendGetRequest("friends?token=" + application.api.User.getLoginToken()));
+
+	public static ObservableList<User> getFriends() throws JSONException, IOException
+	{
+		if (friends == null)
+		{
+			JSONObject response = new JSONObject(
+					HttpWebRequest.sendGetRequest("friends?token=" + application.api.User.getLoginToken()));
 			JSONArray friendArr = response.getJSONArray("friends");
 			int friendAmount = friendArr.length();
 			friends = FXCollections.observableArrayList();
-			for (int i = 0; i < friendAmount; i++) {
+			for (int i = 0; i < friendAmount; i++)
+			{
 				JSONObject curFriend = friendArr.getJSONObject(i).getJSONObject("friend_user");
-				if (curFriend.has("name")) {
+				if (curFriend.has("name"))
+				{
 					friends.add(new application.model.User(curFriend.getString("name"), curFriend.getInt("id")));
 				}
 			}
 		}
 		return friends;
 	}
-	
-	public static boolean acceptRequest(int requestID) throws JSONException, IOException {
-		JSONObject response = new JSONObject(HttpWebRequest.sendGetRequest("friend/" + requestID + "/accept?token=" + application.api.User.getLoginToken()));
+
+	public static boolean acceptRequest(int requestID) throws JSONException, IOException
+	{
+		JSONObject response = new JSONObject(HttpWebRequest
+				.sendGetRequest("friend/" + requestID + "/accept?token=" + application.api.User.getLoginToken()));
 		return response.getString("message").equals("Freundschaftsanfrage wurde angenommen.");
 	}
-	
-	public static boolean declineRequest(int requestID) throws JSONException, IOException {
-		JSONObject response = new JSONObject(HttpWebRequest.sendGetRequest("friend/" + requestID + "/decline?token=" + application.api.User.getLoginToken()));
+
+	public static boolean declineRequest(int requestID) throws JSONException, IOException
+	{
+		JSONObject response = new JSONObject(HttpWebRequest
+				.sendGetRequest("friend/" + requestID + "/decline?token=" + application.api.User.getLoginToken()));
 		System.out.println(response);
 		return response.getString("message").equals("Freundschaftsanfrage wurde abgelehnt.");
 	}
-	
 
-
-	public static ObservableList<application.model.FriendRequest> getFriendRequests() throws JSONException, IOException {
-		if (friendRequests == null) {
-			JSONObject response = new JSONObject(HttpWebRequest.sendGetRequest("friend/requests?token=" + application.api.User.getLoginToken()));
-			if (response.has("requests") == true) {
+	public static ObservableList<application.model.FriendRequest> getFriendRequests() throws JSONException, IOException
+	{
+		if (friendRequests == null)
+		{
+			JSONObject response = new JSONObject(
+					HttpWebRequest.sendGetRequest("friend/requests?token=" + application.api.User.getLoginToken()));
+			if (response.has("requests") == true)
+			{
 				JSONArray requests = response.getJSONArray("requests");
 				friendRequests = FXCollections.observableArrayList();
-				for (int i = 0; i < requests.length(); i++) {
+				for (int i = 0; i < requests.length(); i++)
+				{
 					JSONObject curRequest = requests.getJSONObject(i);
-					friendRequests.add(new application.model.FriendRequest(curRequest.getInt("id"), curRequest.getJSONObject("user").getString("name"), curRequest.getJSONObject("user").getInt("id")));
+					friendRequests.add(new application.model.FriendRequest(curRequest.getInt("id"),
+							curRequest.getJSONObject("user").getString("name"),
+							curRequest.getJSONObject("user").getInt("id")));
 				}
 			}
 		}
